11375e8ff5e37008be74568dd69365a04ffb4b2e
Hapyto4y <joedanik@gmail.com>
2025-01-09 10:27:56 -0700

    Fix & test

--- Flashcards/script.js
+++ Flashcards/script.js
@@ -1,1 +1,9 @@
-document.querySelector('.flashcard').addEventListener('click', function() {this.classList.toggle('flipped');})
+// Select the flashcard
+cont flashcard = document.querySelector('.flashcard');
+
+//Add a click event listener to the flashcard
+.addEventListener('click', function() {
+
+//toggle the "flipped" class on the flashcard
+this.classList.toggle('flipped');
+});
