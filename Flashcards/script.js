68612149d3ab1673f0f676ce9f024e1a5901ee96
Hapyto4y <joedanik@gmail.com>
2025-01-10 08:35:04 -0700

    Update script.js

--- Flashcards/script.js
+++ Flashcards/script.js
@@ -1,9 +1,8 @@
 // Select the flashcard
 const flashcard = document.querySelector('.flashcard');
 
-//Add a click event listener to the flashcard
-.addEventListener('click', function() {
-
-//toggle the "flipped" class on the flashcard
-this.classList.toggle('flipped');
+// Add a click event listener to the flashcard
+flashcard.addEventListener('click', function () {
+  // Toggle the "flipped" class on the flashcard
+  this.classList.toggle('flipped');
 });
